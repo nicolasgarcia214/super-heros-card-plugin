72dde384dc149b86ab5e8ff37078abf2fd8f218b
Nicolas Garcia <nicolasgarcia214@gmail.com>
2021-04-04 21:43:17 -0500

    🗃 Folder Structure

---
+++ superhero-card.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @since             1.0.0
+ * @package           SuperHeroCard
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Super Hero Card
+ * Description:       This plugin allows editors to embed a Superhero card with the image, abilities, and biography on a WordPress post/page.
+ * Version:           1.0.0
+ * Author:            Nicolas Garcia
+ * Author URI:        https://nicolasgarcia214.site
+ * License:           GPL-3.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+ // If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
